e614a95c723801fcf7c203d898db5371833f765e
Jeronimo <jeronimotornini@gmail.com>
2026-04-07 16:48:07 -0300

    Update metadata and copyright

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -1,7 +1,7 @@
 export default function Copyright() {
   return (
     <div className="w-full text-center py-4">
-      <p className="text-sm text-gray-500">
+      <p className="text-sm text-stone-500">
         &copy; {new Date().getFullYear()} AIO - All In One. Todos los derechos
         reservados.
       </p>
