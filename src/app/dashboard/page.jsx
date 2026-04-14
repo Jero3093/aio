66681df8eeb57b5a42829cdd19728dc524814193
Jeronimo <jeronimotornini@gmail.com>
2026-04-14 16:20:49 -0300

    Add inventory visualizer on dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,5 +1,7 @@
 import DashboardNav from "@/components/dashboard/DashboardNav";
+import InventoryVisualizer from "@/components/dashboard/InventoryVisualizer";
 import UsersVisualizer from "@/components/dashboard/UsersVisualizer";
+import useInventory from "@/hooks/useInventory";
 import useSession from "@/hooks/useSession";
 import useUsers from "@/hooks/useUsers";
 import { redirect } from "next/navigation";
@@ -14,13 +16,18 @@ export default async function Dashboard() {
     forVisualizer: true,
   });
 
+  const inventory = await useInventory({
+    companyId: session?._id,
+    forVisualizer: true,
+  });
+
   return (
     <main className="w-full max-h-screen flex flex-row gap-10 py-6">
       <DashboardNav />
       <section className="w-full max-h-screen p-6 overflow-y-scroll overflow-hidden">
         <h2 className="text-4xl font-bold">Dashboard</h2>
         <UsersVisualizer users={users.reverse()} />
-        <section className="w-full h-56 bg-stone-800 rounded-md mt-6 flex items-center justify-center"></section>
+        <InventoryVisualizer inventory={inventory} />
         <section className="w-full h-56 bg-stone-800 rounded-md mt-6 flex items-center justify-center"></section>
       </section>
     </main>
